b8f696b4a7f5b482d7de039de0646b870808fb2c
VickyForginieri <vforginieri@udesa.edu.ar>
2024-09-16 17:27:06 -0300

    Primera parte cardpelicula

--- src/components/CardPelicula/CardPelicula.js
+++ src/components/CardPelicula/CardPelicula.js
@@ -1,1 +1,33 @@
 /*Mirar ejercicios de la clase 6bis --> Vicky*/ 
+import React, {Component} from "react";
+
+class CardPelicula extends Component {
+    constructor() {
+        super();
+        this.state = {
+            descripcion: null
+        }};
+
+    //mostrarDescripcion(){
+        //this.setState(){
+        //    descripcion 
+      //  }
+    //};
+
+    render(){
+        const {pelicula} = this.props;
+        return(
+            <>
+            <img src={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`}
+          alt={pelicula.title}
+        />
+        <h1>{pelicula.title}</h1>
+        
+        </>
+        )
+    };
+    
+
+}
+
+export default CardPelicula;
