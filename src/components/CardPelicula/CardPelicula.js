81e1c5b616517694fa83431f6fd589b938b3bf5c
VickyForginieri <vforginieri@udesa.edu.ar>
2024-09-19 10:53:26 -0300

    punto 3, falta favoritos

--- src/components/CardPelicula/CardPelicula.js
+++ src/components/CardPelicula/CardPelicula.js
@@ -1,32 +1,44 @@
-/*Mirar ejercicios de la clase 6bis --> Vicky*/ 
-import React, {Component} from "react";
+/*Mirar ejercicios de la clase 6bis --> Vicky*/
+import React, { Component } from "react";
 
 class CardPelicula extends Component {
     constructor() {
         super();
         this.state = {
-            descripcion: null
-        }};
+            verDescripcion: false,
+            esFavorito: false
+        }
+    };
 
-    //mostrarDescripcion(){
-        //this.setState(){
-        //    descripcion 
-      //  }
-    //};
+    verDescripcion() {
+        this.setState({
+            verDescripcion: !this.state.verDescripcion
+        })
+    }
 
-    render(){
-        const {pelicula} = this.props;
-        return(
+    render() {
+        const { pelicula} = this.props;
+        return (
             <>
-            <img src={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`}
-          alt={pelicula.title}
-        />
-        <h1>{pelicula.title}</h1>
-        
-        </>
+                <img src={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`}
+                    alt={pelicula.title}
+                />
+                <h1>{pelicula.title}</h1>
+                <p onClick={() => this.verDescripcion()}>
+                    {this.state.verDescripcion ? "Ocultar Descripción" : "Ver Descripción"}
+                </p>
+                {this.state.verDescripcion && (
+                    <section>
+                        <p>{pelicula.overview} </p>
+                    </section>
+                )}
+                <a href={`/detalle/id/${pelicula.id}`}>Detalle</a> {/*cambiar a ruta correspondiente*/}
+
+                
+            </>
         )
     };
-    
+
 
 }
 
